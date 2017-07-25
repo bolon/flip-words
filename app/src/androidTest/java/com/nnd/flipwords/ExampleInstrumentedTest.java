4d09d2f85658b58185a2503ac47faf38647d9738
bolon <fsimbolon@dyned.com>
2017-07-25 15:38:06 +0700

    update pkg name

--- app/src/androidTest/java/com/nnd/flipwords/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nnd/flipwords/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.nnd.myapplication", appContext.getPackageName());
+        assertEquals("com.nnd.flipwords", appContext.getPackageName());
     }
 }
